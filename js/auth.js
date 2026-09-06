5eddc3c14ea0689f7e05a48d10a75d370d477c52
Z13r0 <se.cerdab@duocuc.cl>
2026-09-05 21:30:19 -0400

    actualización

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // ======================================================
 // AUTH.JS
 // Maneja:
@@ -244,25 +244,3 @@ function crearAdminInicial() {
         );
     }
 }
-=======
-//Datos de prueba
-const emailUserTest = "usuario@gmail.com"
-const passUserTest = "1234"
-const emailAdminTest = "admin@admin.cl"
-const passAdminTest = "1234"
-
-// Boton Ingresar
-const botonIngresar = document.querySelector('#ingresar');
-botonIngresar.addEventListener('click', (e) =>{
-    e.preventDefault();
-    const email = document.querySelector('#email').value;
-    const pass = document.querySelector('#password').value;
-    if (email == emailUserTest && pass == passUserTest){
-        window.location = "index.html";
-    } else if (email == emailAdminTest && pass == passAdminTest){
-        window.location = "admin.html";
-    } else {
-        alert("Datos Incorrectos.");
-    }
-})
->>>>>>> ceccd56b5f8796b1bc8b91e9799216e2a2e4b1c0
